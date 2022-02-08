900b95904185d276eaa54e136ac528437e474298
Eliane Maalouf <eliane.maalouf@hefr.ch>
2022-02-08 19:56:04 +0000

    Comb_part1 exercises

---
+++ notebooks/Combinatoires/fn.py
@@ -0,0 +1,39 @@
+def verify_ex1(n, n_choice):
+    total = n_choice ** n 
+    return total 
+
+def verify_ex2(n_arome, agencement = [1,2,3]):
+    total = 0
+    for i in range(len(agencement)):
+        total = total + ex2_count(n_arome,agencement[i])
+    return total 
+
+def ex2_count(n_arome, boules):
+    # pour un type d'agencement fixé
+    total = 1
+    while boules > 0:
+        total = total * n_arome
+        n_arome -= 1
+        boules -= 1
+    
+    return total 
+
+def verify_ex3_1():
+    total = 9 * 9 * 8 * 7
+    return total 
+
+def verify_ex3_2():
+    total = 8 * 8 * 7 * 6
+    return total 
+
+def verify_ex3_3():
+    total = 9 * 8 * 7 + 8 * 8 * 7 
+    return total 
+
+def verify_ex3_4():
+    total = 5 * 9 * 8 + 5 * 9 * 8 * 7 + 5 * 9 * 8 * 7 * 6
+    return total 
+
+def count_total(n, n_choice, n_correct):
+    total = n_choice**n
+    return total
